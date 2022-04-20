91c87881643f1310984c3870b1c093f707653062
Murray918 <andrew.murray@favordelivery.com>
2022-04-20 17:20:02 -0500

    add support for cors reverse proxy fix react implementation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css';
 import { useCallback, useEffect, useState } from 'react';
 
 function App() {
+   const initialState = {
+    businesses: [],
+    reigon: [],
+    total: 0
+  }
 
   const searchParam = 'rice'
   const location = {
@@ -10,39 +15,30 @@ function App() {
     longitude: null
   }
  // eslint-disable-next-line no-unused-vars
- const [restaurants, setRestaurants] = useState(null)
+ const [restaurants, setRestaurants] = useState(initialState)
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  const callYelpApiWithCredentials = useCallback(async (searchParam, location) => {
-     const requestUrl = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search'
-    try {
-      const response = await fetch(`${requestUrl}?term=${searchParam}&location=NYC`, {
-        headers: {
-          'Authorization': `Bearer ${process.env.REACT_APP_YELP_API_KEY}`
-        }
-    })
-    const restaurantsResponse = response.json()
-    console.log(restaurantsResponse)
-    setRestaurants(restaurantsResponse)
+  useEffect( () => {
+    const callYelpApiWithCredentials = async (searchParam, location) => {
+      const requestUrl = 'http://127.0.0.1:4000/https://api.yelp.com/v3/businesses/search'
+     try {
+       const response = await fetch(`${requestUrl}?term=${searchParam}&location=NYC`, {
+         headers: {
+           'Authorization': `Bearer ${process.env.REACT_APP_YELP_API_KEY}`,
+         }
+     })
+     const restaurantsResponse = await response.json()
+     console.log(restaurantsResponse)
+     setRestaurants(restaurantsResponse)
 
-    } catch (error) {
-      console.error(error)
-    }
-})
+     } catch (error) {
+       console.error(error)
+     }
+ }
 
-// this is a formatter that you could use to format the location if you got it from the browser
-// if this does not make sense than we can go through what the code is doing
- // eslint-disable-next-line no-unused-vars
- function formatLocationToUrlEncode(locationObject) {
-   const objectKeys = Object.keys(locationObject)
-   objectKeys.map((key, index) => {
-      if (locationObject[key]) return `${key}=${locationObject[key]}`
-   })
+    callYelpApiWithCredentials(searchParam, location)
 
-}
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(callYelpApiWithCredentials(searchParam, location), [])
+}, [])
 
   return (
     <div className="App">
@@ -50,10 +46,10 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
       </header>
       <ul>
-      {restaurants.buisnesses.map(businesses => {
-          return <li>
-            {businesses.name}
-          </li>
+      {restaurants.businesses.map((business, index) => {
+          return (<li key={`${business.name}`}>
+            {business.name}
+          </li>)
         })}
       </ul>
     </div>
